8e3e0f971e9cc79c2d17aba754e29cbc140a6bfb
arthurBricq <arthur.bricq@epfl.ch>
2018-07-26 13:05:29 +0200

    ajout du nouveau storyboard

---
+++ VC_Tutorial/MenuTutorialViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MenuTutorialViewController.swift
+//  Demineur
+//
+//  Created by Arthur BRICQ on 26/07/2018.
+//  Copyright © 2018 Arthur BRICQ. All rights reserved.
+//
+
+import UIKit
+
+class MenuTutorialViewController: UIViewController {
+
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
