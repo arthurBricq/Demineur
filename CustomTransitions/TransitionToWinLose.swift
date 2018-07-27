a3cf1de1a14dd3a28c9b189225b05a3a26189c5d
Moi <you@example.com>
2018-07-27 12:23:46 +0200

    Correction du bug avec la transition du WinLooseViewController

--- CustomTransitions/TransitionToWinLose.swift
+++ CustomTransitions/TransitionToWinLose.swift
@@ -29,7 +29,7 @@ class TransitionToWinLose: NSObject, UIViewControllerAnimatedTransitioning {
         transitionContext.containerView.addSubview(toView)
         
         let lineFrame = toVC.lineView.frame
-        toVC.lineView.frame = CGRect(x: lineFrame.minX, y: lineFrame.minY-toView.frame.height, width: lineFrame.width, height: lineFrame.height)
+        toVC.lineView.frame = CGRect(x: lineFrame.minX, y: lineFrame.minY-toView.frame.height, width: lineFrame.width, height: toView.frame.height)
         let eFrame = toVC.letterE.frame
         toVC.letterE.frame = CGRect(x: eFrame.minX, y: eFrame.minY-toView.frame.height, width: eFrame.width, height: eFrame.height)
         let nFrame = toVC.letterN.frame
@@ -44,6 +44,8 @@ class TransitionToWinLose: NSObject, UIViewControllerAnimatedTransitioning {
         toVC.replayIconButton.alpha = 0
         toVC.lastButton.alpha = 0
         
+        let correctiveConstant: CGFloat = isItABigScreen() ? -24 : 20
+        
         UIView.animateKeyframes(withDuration: self.animationDuration, delay: 0.6, options: [], animations: {
 
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.2, animations: {
@@ -51,10 +53,10 @@ class TransitionToWinLose: NSObject, UIViewControllerAnimatedTransitioning {
             })
             
             UIView.addKeyframe(withRelativeStartTime: 0.15, relativeDuration: 0.55, animations: {
-                toVC.lineView.frame = CGRect(x: lineFrame.minX, y: lineFrame.minX, width: lineFrame.width, height: lineFrame.height)
-                toVC.letterE.frame = CGRect(x: eFrame.minX, y: eFrame.minY, width: eFrame.width, height: eFrame.height)
-                toVC.letterN.frame = CGRect(x: nFrame.minX, y: nFrame.minY, width: nFrame.width, height: nFrame.height)
-                toVC.letterD.frame = CGRect(x: dFrame.minX, y: dFrame.minY, width: dFrame.width, height: dFrame.height)
+                toVC.lineView.frame = CGRect(x: lineFrame.minX, y: lineFrame.minY, width: lineFrame.width, height: toView.frame.height)
+                toVC.letterE.frame = CGRect(x: eFrame.minX, y: eFrame.minY-correctiveConstant, width: eFrame.width, height: eFrame.height)
+                toVC.letterN.frame = CGRect(x: nFrame.minX, y: nFrame.minY-correctiveConstant, width: nFrame.width, height: nFrame.height)
+                toVC.letterD.frame = CGRect(x: dFrame.minX, y: dFrame.minY-correctiveConstant, width: dFrame.width, height: dFrame.height)
             })
             
             UIView.addKeyframe(withRelativeStartTime: 0.55, relativeDuration: 0.45, animations: {
