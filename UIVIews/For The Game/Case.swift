a2f6f06542144cd92391ea6c7b850d29e1625552
arthurBricq <arthur.bricq@epfl.ch>
2018-11-25 17:37:29 -0800

    Just added two abstract classes (for the view of game and for the cases). In many ways, I tried to improve the code and make it more readable. - Now needs to rewrite square case, hex case, triangle case so that it works fine - Also needs to write XXXViewOfGame so that everything fit nicely The plan for now on is to starts with square, then do it for triangle, and finally do it for hex case

---
+++ UIVIews/For The Game/Case.swift
@@ -0,0 +1,217 @@
+//
+//  Case.swift
+//  Demineur
+//
+//  Created by Arthur BRICQ on 25/11/2018.
+//  Copyright © 2018 Arthur BRICQ. All rights reserved.
+//
+
+import UIKit
+
+
+/// This class represents one abstract case  (it is never to be instantiated)
+/// Methods that needs to be overriden: draw method (plus properties)
+class Case: UIButton {
+    
+    // MARK: - Variables
+    
+    var i: Int = 0
+    var j: Int = 0
+    var game: OneGame?
+    var gameState = [[Int]].init()
+    var viewOfGame: ViewOfGame?
+    var markingTimer = LimitedTimer()
+    var option1Timer = LimitedTimer()
+    var caseState: CaseState = .empty  {
+        didSet {
+            if game!.option1 && caseState == .open {
+                option1Timer.start(limit: TimeInterval(game!.option1Time), id: "ReturnCase")
+            }
+            if caseState == .marked {
+                addFlagToCase(flagColor: UIColor.orange)
+            }
+            if caseState == .markedByComputer {
+                addFlagToCase(flagColor: colorForRGB(r: 60, g: 160, b: 100))
+            }
+            if caseState == .empty {
+                removeFlag()
+            }
+        }
+    }
+
+    // MARK: - Inits
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) { fatalError("This class does not support NSCoding") }
+    
+    convenience init(frame: CGRect, game: OneGame, i: Int, j: Int, gameState: [[Int]], viewOfGame: ViewOfGame) {
+        self.init(frame: frame)
+        self.game = game
+        self.i = i
+        self.j = j
+        self.gameState = gameState
+        self.viewOfGame = viewOfGame
+        markingTimer.delegate = self
+    }
+    
+    // MARK: - Function when tapped
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        if caseState == .none || caseState == .open { return }
+        alpha = 0.4
+        if isTheGameStarted.value == false {
+            isTheGameStarted.delegate!.createTheGame(withFirstTouched: (i,j)) // le jeu est maintenant crée.
+            isTheGameStarted.value = true
+            return
+        }
+        // Starts the marking time
+        markingTimer.start(limit: TimeInterval(dataManager.giveTimeToMantain()), id: "Marking")
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        super.touchesEnded(touches, with: event)
+        if caseState == .none || caseState == .open { return }
+        UIView.animate(withDuration: 0.1) {
+            self.alpha = 1.0
+        }
+        // ********* Logique lorsqu'on appuit sur un boutton ********** //
+        if isUserInteractionEnabled { // Cela veut dire que le timer ne s'est pas encore terminé.
+            markingTimer.stop()
+            // on appelle la fonction pour retourner les cases
+            viewOfGame!.buttonHaveBeenTapped(i: i, j: j, marking: false)
+        } else {
+            isUserInteractionEnabled = true
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        // Stopping the timer
+        markingTimer.stop()
+        UIView.animate(withDuration: 0.1) {
+            self.alpha = 1.0
+        }
+    }
+    
+    // MARK: - Functions
+
+    private func addFlagToCase(flagColor: UIColor) {
+        let flag = FlagView(frame: bounds, circleCenter: CGPoint(x: bounds.width/2, y: bounds.height/2), r: 0.2*bounds.width, color: flagColor)
+        addSubview(flag)
+        Vibrate().vibrate(style: .medium)
+    }
+
+    private func removeFlag() {
+        for subview in subviews {
+            if subview is FlagView {
+                let flag = subview as! FlagView
+                flag.removeFromSuperview()
+                Vibrate().vibrate(style: .light)
+            }
+        }
+    }
+    
+    /// Quand la partie se termine, gère les animations de la case.
+    func animateGameOver(win: Bool, bombTapped: Bool = false) {
+        if win {
+            for subview in subviews {
+                // Pour le drapeau
+                if subview is FlagView {
+                    // si la case était une bombe
+                    if gameState[i][j] == -1 {
+                        // La case était marquée et était une bombe
+                        let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+                        scaleAnimation.values = [1.0, 0.9, 1.7, 1.1]
+                        scaleAnimation.keyTimes = [0, 0.2, 0.6, 1]
+                        scaleAnimation.duration = 0.5
+                        scaleAnimation.isRemovedOnCompletion = false
+                        scaleAnimation.fillMode = kCAFillModeForwards
+                        subview.layer.add(scaleAnimation, forKey: nil)
+                    } else {
+                        // La case était marquée et n'était pas une bombe
+                        let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+                        scaleAnimation.values = [1.0, 1.3, 0]
+                        scaleAnimation.keyTimes = [0, 0.2, 1]
+                        scaleAnimation.duration = 0.5
+                        scaleAnimation.isRemovedOnCompletion = false
+                        scaleAnimation.fillMode = kCAFillModeForwards
+                        subview.layer.add(scaleAnimation, forKey: nil)
+                    }
+                }
+            }
+        } else {
+            // La partie est perdue
+            // si la case est celle sur laquelle le joueur a tappé
+            if bombTapped {
+                let cross = BombView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height), percentOfCase: 0.4, lineWidth: 4, color: UIColor.red)
+                self.addSubview(cross)
+                
+                let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+                scaleAnimation.fromValue = 2.3
+                scaleAnimation.toValue = 1
+                scaleAnimation.duration = 0.5
+                scaleAnimation.timingFunction = CAMediaTimingFunction(controlPoints: 0.4, -0.1, 0.4, 0.5)
+                scaleAnimation.fillMode = kCAFillModeBackwards
+                cross.layer.add(scaleAnimation, forKey: nil)
+            } else {
+                // si la case était marquée
+                if caseState == .marked || caseState == .markedByComputer {
+                    for subview in subviews {
+                        if subview is FlagView {
+                            let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+                            scaleAnimation.values = [1.0, 1.3, 0]
+                            scaleAnimation.keyTimes = [0, 0.2, 1]
+                            scaleAnimation.duration = 0.5
+                            scaleAnimation.isRemovedOnCompletion = false
+                            scaleAnimation.fillMode = kCAFillModeForwards
+                            subview.layer.add(scaleAnimation, forKey: nil)
+                        }
+                    }
+                }
+                // si la case était une bombe
+                if gameState[i][j] == -1 {
+                    var color: UIColor?
+                    if caseState == .marked || caseState == .markedByComputer {
+                        color = colorForRGB(r: 100, g: 200, b: 150)
+                    } else {
+                        color = UIColor.red
+                    }
+                    let cross = BombView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height), percentOfCase: 0.4, lineWidth: 4, color: color!)
+                    self.addSubview(cross)
+                    let alphaAnimation = CABasicAnimation(keyPath: "opacity")
+                    alphaAnimation.fromValue = 0
+                    alphaAnimation.toValue = 1
+                    alphaAnimation.duration = 0.5
+                    alphaAnimation.timingFunction = CAMediaTimingFunction(controlPoints: 0.5, 0, 0.7, 0.4)
+                    cross.layer.add(alphaAnimation, forKey: nil)
+                }
+                
+            }
+            
+        }
+    }
+}
+
+extension Case: LimitedTimerProtocol {
+    func timeLimitReached(id: String) {
+        if id == "Marking" {
+            viewOfGame!.buttonHaveBeenTapped(i: i, j: j, marking: true)
+            markingTimer.stop()
+            // Block the case
+            isUserInteractionEnabled = false
+        } else if id == "ReturnCase" { /// OPTION 1 HAPPENS HERE.
+            if self.caseState == .open {
+                self.caseState = .empty
+                if self.subviews.count > 0 {
+                    self.subviews[0].removeFromSuperview()
+                }
+                setNeedsDisplay()
+            }
+        }
+    }
+}
---
+++ UIVIews/For The Game/ViewOfGame.swift
@@ -0,0 +1,278 @@
+//
+//  ViewOfGame.swift
+//  Demineur
+//
+//  Created by Arthur BRICQ on 25/11/2018.
+//  Copyright © 2018 Arthur BRICQ. All rights reserved.
+//
+
+import UIKit
+
+/// This class is responsible to handle a game, of any type and for any mode of party.
+/// In practise, you never get to instantiate an element of this class (use child of the class) because we don't provide implementation of the draw methods here.
+/// Methods to be overriden: instantiateCases, ouvertureRecursive
+
+class ViewOfGame: UIView {
+    
+    // MARK: - Variables
+    
+    var game: OneGame?
+    var delegate: GameViewCanCallVC?
+    /// Holds the position of bombs and the numbers displayed when cases are opened.
+    /// Keeps in mind that this array is only modified when one game is created.
+    var gameState: [[Int]] = [[]]
+    /// Holds all the cases present on screen.
+    var cases: [[Case]] = [[]]
+    /// Holds the number of remaining flags.
+    var numberOfFlags: Int = 5 {
+        didSet {delegate?.updateFlagsDisplay(numberOfFlags: numberOfFlags)}
+    }
+    /// Permet de compter le nombre total de drapeaux correct que le joueur a posé. Cette closure va rajouter +1 à la variable 'numberOfBombs' du VC si on marque un drapeau au bon endroit.
+    var onPosingFlag: ((Bool) -> Void)?
+    /// Permet de compter le nombre total de drapeaux correct que le joueur a posé. Cette closure va enlever -1 à la variable 'numberOfBombs' du VC si on enlève un drapeau correct.
+    var onUnposingFlag: ((Bool) -> Void)?
+    
+    // MARK: - Inits functions
+    
+    override init(frame: CGRect) { super.init(frame: frame) }
+    
+    required init(coder aDecoder: NSCoder) { fatalError("This class does not support NSCoding") }
+ 
+    convenience init(frame: CGRect, game: OneGame) {
+        self.init(frame: frame)
+        self.game = game
+        instantiateCases()
+    }
+    
+    // MARK: - Functions
+    
+    /// This is method is called by cases when user taps on them
+    public func buttonHaveBeenTapped(i: Int, j: Int, marking: Bool) { // this function has all the logic whenever a button is tapped.
+        
+        if isCaseNone(i: i, j: j) || isTheCaseOpen(i: i, j: j) || isCaseBlocked(i: i, j: j) {
+            return
+            
+        }
+        
+        if marking { // hold tapping --> have to mark or unmark the card
+            if !isTheCaseMarked(i: i, j: j) {
+                let test = isCaseABomb(i: i, j: j)
+                self.onPosingFlag?(test)
+                markACaseAt(i: i, j: j)
+            } else {
+                self.onUnposingFlag?(isCaseABomb(i: i, j: j))
+                unmarkACaseAt(i: i, j: j)
+            }
+        } else { // Quick tapping --> have to open the case
+            if !isTheCaseMarked(i: i, j: j) { // si la case n'est pas marquée.
+                if isCaseABomb(i: i, j: j) {
+                    // Le joueur a tapé sur une bombe
+                    delegate?.gameOver(win: false, didTapABomb: true, didTimeEnd: false)
+                    callEndAnimation(onButtonAt: i, j: j, win: false, bombTapped: true)
+                } else {
+                    ouvertureRecursive(line: i, column: j) // To open
+                }
+            }
+        }
+        
+        if isTheGameFinished() { // end of game
+            delegate!.gameOver(win: true, didTapABomb: false, didTimeEnd: false)
+            returnAllTheCases(win: true)
+        }
+        
+    }
+    
+    private func isTheGameFinished() -> Bool {
+        var toReturn = true
+        for i in 0..<gameState.count {
+            for j in 0..<gameState[0].count {
+                // We check if all bombs are marked
+                if isCaseABomb(i: i, j: j) {
+                    if !isTheCaseMarked(i: i, j: j) {
+                        toReturn = false
+                    }
+                }
+                
+            }
+        }
+        
+        return toReturn
+    }
+    
+    private func isTheCaseMarked(i: Int, j: Int) -> Bool {
+        return cases[i][j].caseState == .marked
+    }
+    
+    private func isTheCaseOpen(i: Int, j: Int) -> Bool {
+        return cases[i][j].caseState == .open
+    }
+    
+    private func isCaseNone(i: Int, j: Int) -> Bool {
+        return cases[i][j].caseState == .none
+    }
+    
+    private func isCaseBlocked(i: Int, j: Int) -> Bool {
+        return cases[i][j].caseState == .blocked
+    }
+    
+    private func isCaseABomb(i: Int, j: Int) -> Bool {
+        return gameState[i][j] == -1
+    }
+    
+    /// This method needs to be overriden by subclasses.
+    /// It's supposed to dispose the cases in the right position (not on the draw function anymore)
+    private func instantiateCases() {
+        fatalError("This method needs to be overriden by children")
+    }
+    
+    private func ouvertureRecursive(line i: Int, column j: Int) {
+        fatalError("This method needs to be overriden by children")
+
+        /*
+        
+        // **** Constant to use **** //
+        let minI = 0
+        let minJ = 0
+        let maxI = gameState.count-1
+        let maxJ = gameState[0].count-1
+        
+        // **** Variable to use **** //
+        var hasToContinue: Bool = false
+        var hasToOpen: Bool = false
+        
+        // followings condition are true if we should open the case
+        let cond1_1 = (i >= minI && i <= maxI) ; let cond1_2 = (j >= minJ && j <= maxJ);
+        let cond1 = cond1_1 && cond1_2
+        if !cond1 { return } else {
+            let cond2 = !isTheCaseOpen(i: i, j: j)
+            let cond3 = !isTheCaseMarked(i: i, j: j)
+            let cond4 = !isCaseNone(i: i, j: j)
+            hasToOpen = cond1 && cond2 && cond3 && cond4
+            
+            // followings condition are true if we should not continue the recursion
+            if gameState[i][j] == 0 {
+                hasToContinue = true
+            }
+            
+        }
+        
+        if isCaseBlocked(i: i, j: j) { return }
+        
+        // Implementation starts here
+        if hasToOpen {
+            returnACaseAt(i: i, j: j)
+        } else {
+            return
+        }
+        
+        if hasToContinue {
+            ouvertureRecursive(line: i+1, column: j+1)
+            ouvertureRecursive(line: i+1, column: j)
+            ouvertureRecursive(line: i+1, column: j-1)
+            ouvertureRecursive(line: i, column: j-1)
+            ouvertureRecursive(line: i-1, column: j-1)
+            ouvertureRecursive(line: i-1, column: j)
+            ouvertureRecursive(line: i-1, column: j+1)
+            ouvertureRecursive(line: i, column: j+1)
+        } else {
+            return
+        }
+        */
+    }
+    
+    /// This method return the case (opens the case)
+    private func returnACaseAt(i: Int, j: Int) {
+        cases[i][j].caseState = .open
+    }
+    
+    private func markACaseAt(i: Int, j: Int, byComputer: Bool = false) {
+        if numberOfFlags <= 0 { return }
+        numberOfFlags = numberOfFlags - 1
+        cases[i][j].caseState = byComputer ? .markedByComputer : .marked
+    }
+    
+    private func unmarkACaseAt(i: Int, j: Int) {
+        cases[i][j].caseState = .empty
+    }
+    
+    private func blockACaseAt(i: Int, j: Int) {
+        if cases[i][j].caseState == .empty {
+            cases[i][j].caseState = .blocked
+        }
+    }
+    
+    private func unblockACaseAt(i: Int, j: Int) {
+        cases[i][j].caseState = .empty
+    }
+    
+    /* I feel like this method is totally useless
+    func updateAllNumbers() {
+        for button in self.subviews {
+            if button is SquareCase {
+                let button = button as! SquareCase
+                button.gameState = gameState
+            }
+        }
+    }
+    */
+    
+    private func returnAllTheCases(win: Bool = false) {
+        for i in 0..<gameState.count {
+            for j in 0..<gameState[i].count {
+                if isCaseABomb(i: i, j: j) {
+                    callEndAnimation(onButtonAt: i, j: j, win: win)
+                } else if isTheCaseMarked(i: i, j: j) {
+                    callEndAnimation(onButtonAt: i, j: j, win: false)
+                }
+            }
+        }
+    }
+    
+    private func callEndAnimation(onButtonAt i: Int, j: Int, win: Bool, bombTapped: Bool = false) {
+        cases[i][j].animateGameOver(win: win, bombTapped: bombTapped)
+    }
+    
+    
+    /**
+     Cette fonction est appelé pour le bonus 'bombe' afin de retourner une bombe aléatoire
+     */
+    public func markARandomBomb() {
+        // une chance sur deux de partir du haut
+        let cas = random(2) + 1
+        let n = gameState.count
+        for i in 0..<n {
+            let m = gameState[i].count
+            for j in 0..<m {
+                switch cas {
+                case 1:
+                    if isCaseABomb(i: i, j: j) {
+                        if !isTheCaseMarked(i: i, j: j) {
+                            markACaseAt(i: i, j: j, byComputer: true)
+                            // si on gagne la partie à l'aide du bonus ...
+                            if isTheGameFinished() { // end of game
+                                delegate!.gameOver(win: true, didTapABomb: false, didTimeEnd: false)
+                                returnAllTheCases(win: true)
+                            }
+                            return
+                        }
+                    }
+                case 2:
+                    if isCaseABomb(i: n-i-1, j: m-j-1) {
+                        if !isTheCaseMarked(i: n-i-1, j: m-j-1) {
+                            markACaseAt(i: n-i-1, j: m-j-1, byComputer: true)
+                            if isTheGameFinished() { // end of game
+                                delegate!.gameOver(win: true, didTapABomb: false, didTimeEnd: false)
+                                returnAllTheCases(win: true)
+                            }
+                            return
+                        }
+                    }
+                default:
+                    break
+                }
+            }
+        }
+   
+    }
+    
+}
