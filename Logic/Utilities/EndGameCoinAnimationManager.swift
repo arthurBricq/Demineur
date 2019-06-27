6a11878c0e55adde6e69248f2f1b2d99660cad57
Marin Bricq <bricqmarin1790@gmail.com>
2019-06-27 13:31:52 +0200

    Animation pour une case faite

--- Logic/Utilities/EndGameCoinAnimationManager.swift
+++ Logic/Utilities/EndGameCoinAnimationManager.swift
@@ -12,6 +12,7 @@ import UIKit
 class EndGameCoinAnimationManager: NSObject {
     
     var gameView: ViewOfGame
+    let coinsEarnedPerBomb: Int = 10
     
     init(gameViewToAnimate: ViewOfGame) {
         self.gameView = gameViewToAnimate
@@ -38,10 +39,58 @@ class EndGameCoinAnimationManager: NSObject {
         
     }
     
-    public func test() {
-        for a in returnAllCorrectlyMarkedBombs() {
-            print("i : \(a.i); j : \(a.j)")
+    /// This function will animate the case that it has as a parameter. It creates a message with the coins earned and makes it move
+    public func animateOneCase(toAnimate: Case) {
+        
+        let randomXTranslation = (CGFloat(random(Int(toAnimate.frame.width))) - toAnimate.frame.width/2)
+        let msgToAnimate = returnMessageWithCoinsEarned(rect: CGRect(x: toAnimate.frame.midX + randomXTranslation - 80/2, y: toAnimate.frame.minY - 10, width: 80, height: 30))
+        msgToAnimate.backgroundColor = UIColor.clear
+        msgToAnimate.alpha = 0
+        gameView.addSubview(msgToAnimate)
+        
+        UIView.animateKeyframes(withDuration: 1.5, delay: 0, options: [], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.2, animations: {
+                msgToAnimate.alpha = 1
+                msgToAnimate.frame = CGRect(x: toAnimate.frame.midX + randomXTranslation - 80/2, y: toAnimate.frame.minY - 15, width: 80, height: 30)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.6, animations: {
+                msgToAnimate.frame = CGRect(x: toAnimate.frame.midX + randomXTranslation - 80/2, y: toAnimate.frame.minY - 30, width: 80, height: 30)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.8, relativeDuration: 0.2, animations: {
+                msgToAnimate.alpha = 0
+                msgToAnimate.frame = CGRect(x: toAnimate.frame.midX + randomXTranslation - 80/2, y: toAnimate.frame.minY - 35, width: 80, height: 30)
+            })
+            
+        }) { (_) in
+            msgToAnimate.removeFromSuperview()
         }
+        
+    }
+    
+    private func returnMessageWithCoinsEarned(rect: CGRect) -> UIView {
+        
+        
+        let message: UIView = UIView(frame: rect)
+        
+        let text = "+\(coinsEarnedPerBomb)"
+        let font = UIFont(name: "PingFangSC-Regular", size: 20)!
+        let lblWidth: CGFloat = (2/3)*rect.width
+        let lblHeight = rect.height
+        let lbl = UILabel(frame: CGRect(x: 0, y: rect.height/2 - lblHeight/2, width: lblWidth, height: lblHeight))
+        lbl.text = text
+        lbl.font = font
+        lbl.textColor = UIColor(red: 204/255, green: 163/255, blue: 0/255, alpha: 1)
+        message.addSubview(lbl)
+        
+        let coinView = PieceView(frame: CGRect(x: text.width(withConstrainedHeight: lblHeight, font: font), y: 0, width: rect.height, height: rect.height))
+        coinView.backgroundColor = UIColor.clear
+        message.addSubview(coinView)
+        
+        return message
+        
     }
     
 }
